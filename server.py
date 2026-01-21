c4666f30b4a530924d77f42efa50293cd347f101
ranchboi <agranshtest@gmail.com>
2026-01-20 22:51:27 -0500

    website commit 1

---
+++ server.py
@@ -0,0 +1,24 @@
+import http.server
+import socketserver
+import os
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        super().end_headers()
+    
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return super().do_GET()
+
+PORT = 8000
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
+    print(f"Server running at http://localhost:{PORT}/")
+    print("Press Ctrl+C to stop")
+    httpd.serve_forever()
+
+
